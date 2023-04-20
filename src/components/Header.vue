1c24c5e9ed1ec64ffb651e83ec0ae809721be490
David Podeszwa <davi.podeszwa@gmail.com>
2023-04-20 18:50:31 +0200

    fixed router-link css selector

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -66,13 +66,13 @@
     justify-content: flex-end;
   }
   
-  .nav-links router-link {
+  .nav-links a {
     margin-right: 16px;
     text-decoration: none;
     color: #333;
   }
   
-  .nav-links router-link.active {
+  .nav-links a.active {
     font-weight: bold;
   }
   </style>
