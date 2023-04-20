c16a9957e70d7c3de7364bf2dafce94563dd70b2
David Podeszwa <davi.podeszwa@gmail.com>
2023-04-20 06:23:26 +0200

    removed some comments

--- src/views/StoresView.vue
+++ src/views/StoresView.vue
@@ -1,23 +1,6 @@
 <template>
     <div class="stores-list">
         <Spinner class="load-spinner" v-if="storesStore.isLoading" />
-        <!-- <div class="store-block" v-for="store in storesStore.activeStores" :key="store.storeID">
-            <RouterLink class="store-info" :to="{name:'search', query: {storeID: store.storeID}}">
-                <div  >
-                    <p>{{ store.storeName }}</p>
-                    <img :src="getStoreThumb(store.images.logo)" />
-                </div>
-            </RouterLink>
-            <div class="deals-list">
-                <Spinner class="load-spinner" v-if="store.isLoading" />
-                <a class="deal-item" v-for="deal in store.deals" :key="deal.dealID" :href="'https://www.cheapshark.com/redirect?dealID='+deal.dealID" target="_blank">
-                    <img :src="deal.thumb" />
-                    <p class="title">{{ deal.title }}</p>
-                    <p class="new-price">{{ deal.salePrice }} $</p>
-                    <p class="normal-price">{{ deal.normalPrice }} $</p>
-                </a>
-            </div>
-        </div> -->
         <GroupedDealsListBlock v-for="store in storesStore.activeStores" :key="store.storeID" 
         :title="store.storeName" :thumbnailSrc="getStoreThumb(store.images.logo)" :get-deal-item-link="deal => deal.thumb"
         :headerNavigationLink="'/search'" :headerNavigationQuery="{storeID: store.storeID}" 
